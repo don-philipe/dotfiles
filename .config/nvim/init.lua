afd41cae419d363e2d49198d31133e1a0074b3e3
don philipe <don_philipe@gmx.de>
2025-02-05 12:34:13 +0100

    Add gitsigns and lualine plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,3 +10,19 @@ require("config.gen-private")
 require("config.lsp")
 require("config.mason")
 require("config.treesitter")
+require("config.gitsigns")
+require("config.lualine")
+
+-- for some reason this must be placed in init.lua file
+-- it doesn't work in any lua/config/*.lua files
+require('lspconfig').pylsp.setup {
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    maxLineLength = 120,
+                },
+            },
+        },
+    },
+}
---
+++ .config/nvim/lua/config/gen-private.lua
@@ -0,0 +1,5 @@
+return {
+    model = "llama3.2",
+    host = "foo",
+    port = "11343",
+}
