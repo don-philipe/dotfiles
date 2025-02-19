be00da9268e11cbbaf45a24511ee21f77bd2536a
don philipe <don_philipe@gmx.de>
2025-02-19 11:21:28 +0100

    Custom nvim gen command adding https schema

--- .config/nvim/lua/plugins/gen.lua
+++ .config/nvim/lua/plugins/gen.lua
@@ -9,5 +9,18 @@ return {
 	      model = vars["model"],
 	      host = vars["host"],
 	      port = vars["port"],
+	      https = vars["https"],
+	      show_prompt = true,
+	      debug = false,
+
+	      -- https://github.com/David-Kunz/gen.nvim/issues/116
+	      command = function(options)
+		      local body = { model = options.model, stream = true }
+		      local schema = "http"
+		      if options.https then
+			      schema = "https"
+		      end
+		      return "curl --silent --no-buffer -X POST " .. schema .. "://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+              end,
       }},
 }
