6f0dc2af553ab90602fe95b6e2179b305b2b0ed4
don philipe <don_philipe@gmx.de>
2025-03-10 12:37:35 +0100

    <leader>h or <leader>H to jump between hunks

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -1,3 +1,5 @@
 require('gitsigns').setup()
 local gitsigns = require('gitsigns')
 vim.keymap.set('n', '<leader>gm', gitsigns.blame_line, { desc = 'Show git message for current line' })
+vim.keymap.set('n', '<leader>h', gitsigns.next_hunk, { desc = 'Move to next hunk' })
+vim.keymap.set('n', '<leader>H', gitsigns.prev_hunk, { desc = 'Move to previous hunk' })
