9803f705e4e7b99a55432c072c507e689a5bd76f
don philipe <don_philipe@gmx.de>
2025-04-08 12:50:46 +0200

    Pin version of nvim plugins lsp and treesitter
    For using named plugins with neovim < 0.10 plugin versions not above a
    certain value are needed.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 return {
-    {'neovim/nvim-lspconfig'},
+    {'neovim/nvim-lspconfig', tag = 'v1.7.0'},  -- pin to a version thats compatible with nvim < 0.10
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
-    {'nvim-treesitter/nvim-treesitter'}
+    {'nvim-treesitter/nvim-treesitter',
+    tag = 'v0.9.3'},  -- pin to a version that works with nvim < 0.10
     -- configure parsers via config/treesitter.lua
     -- update parsers with :TSUpdate
 }
