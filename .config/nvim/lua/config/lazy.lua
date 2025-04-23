7128760315515cd999cbc2a47a97cdcc3a6f6d1b
don philipe <don_philipe@gmx.de>
2025-04-23 22:03:35 +0200

    Add rounded borders for lazy plugin manager

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -33,6 +33,9 @@ require("lazy").setup({
     install = { colorscheme = { "habamax" } },
     -- automatically check for plugin updates
     checker = { enabled = true },
+    ui = {
+	border = "rounded",
+    },
 })
 
 -- Open lazy menu with :Lazy check
