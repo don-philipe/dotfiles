469435454e230fd31baf7d07cc7b4658b6eccb92
don philipe <don_philipe@gmx.de>
2025-05-14 10:17:44 +0200

    Pin mason version

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
+    -- pin version due to https://github.com/mason-org/mason.nvim/issues/1929
     -- mason for downloading language servers
-    {'williamboman/mason.nvim'},
+    {'williamboman/mason.nvim', version = "^1.0.0"},
     -- mason-lspconfig to configure auto setup of language servers
-    {'williamboman/mason-lspconfig.nvim'},
+    {'williamboman/mason-lspconfig.nvim', version = "^1.0.0"},
 }
