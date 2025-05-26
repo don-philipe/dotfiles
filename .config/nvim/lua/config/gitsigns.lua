f84673ed163b23854929823159a66941966d9110
don philipe <don_philipe@gmx.de>
2025-05-26 13:42:02 +0200

    gitsigns.nvim vertical split

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -1,6 +1,6 @@
 require('gitsigns').setup()
 local gitsigns = require('gitsigns')
 vim.keymap.set('n', '<leader>gm', gitsigns.blame_line, { desc = 'Show git message for current line' })
-vim.keymap.set('n', '<leader>gd', ':Gitsigns diffthis<CR>:wincmd w<CR>', { desc = 'Show diff for current line' })
+vim.keymap.set('n', '<leader>gd', ':Gitsigns diffthis ~ vertical=true<CR>:wincmd w<CR>', { desc = 'Show diff for current line' })
 vim.keymap.set('n', '<leader>h', gitsigns.next_hunk, { desc = 'Move to next hunk' })
 vim.keymap.set('n', '<leader>H', gitsigns.prev_hunk, { desc = 'Move to previous hunk' })
