ff7deb9ceefccd88c8ac967bd5fdc07a510dedc9
don philipe <don_philipe@gmx.de>
2025-05-26 13:47:16 +0200

    Shortcut for preview hunk

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<leader>gm', gitsigns.blame_line, { desc = 'Show git messag
 vim.keymap.set('n', '<leader>gd', ':Gitsigns diffthis ~ vertical=true<CR>:wincmd w<CR>', { desc = 'Show diff for current line' })
 vim.keymap.set('n', '<leader>h', gitsigns.next_hunk, { desc = 'Move to next hunk' })
 vim.keymap.set('n', '<leader>H', gitsigns.prev_hunk, { desc = 'Move to previous hunk' })
+vim.keymap.set('n', '<leader>ph', gitsigns.preview_hunk, { desc = 'Preview hunk in popup' })
