6a82e1dc6f43ecd7883a04a3be81a5fa9f575f69
don philipe <don_philipe@gmx.de>
2026-02-17 14:24:19 +0100

    Add detection of ansible files in nvim

--- .config/nvim/lua/config/filetypes.lua
+++ .config/nvim/lua/config/filetypes.lua
@@ -21,3 +21,47 @@ vim.api.nvim_create_autocmd('Filetype', {
 	},
     command = 'setlocal shiftwidth=2 tabstop=2 expandtab'
 })
+if vim.filetype then
+  vim.filetype.add({
+    pattern = {
+      [".*/defaults/.*%.ya?ml"] = "yaml.ansible",
+      [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
+      [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
+      [".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
+      [".*/playbook.*%.ya?ml"] = "yaml.ansible",
+      [".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
+      [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
+      [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+      [".*/tasks/.*%.ya?ml"] = "yaml.ansible",
+      [".*/molecule/.*%.ya?ml"] = "yaml.ansible",
+    },
+  })
+else
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = {
+      "*/defaults/*.yml",
+      "*/defaults/*.yaml",
+      "*/host_vars/*.yml",
+      "*/host_vars/*.yaml",
+      "*/group_vars/*.yml",
+      "*/group_vars/*.yaml",
+      "*/group_vars/*/*.yml",
+      "*/group_vars/*/*.yaml",
+      "*/playbook*.yml",
+      "*/playbook*.yaml",
+      "*/playbooks/*.yml",
+      "*/playbooks/*.yaml",
+      "*/roles/*/tasks/*.yml",
+      "*/roles/*/tasks/*.yaml",
+      "*/roles/*/handlers/*.yml",
+      "*/roles/*/handlers/*.yaml",
+      "*/tasks/*.yml",
+      "*/tasks/*.yaml",
+      "*/molecule/*.yml",
+      "*/molecule/*.yaml",
+    },
+    callback = function()
+      vim.bo.filetype = "yaml.ansible"
+    end,
+  })
+end
